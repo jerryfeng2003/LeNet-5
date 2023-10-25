c7f77c70f80b8d1a83973ef6a9ce235fd5cff89d
jerryfeng2003 <jerryfeng2003@gmail.com>
2023-10-25 11:19:49 +0800

    modify matrix class

--- Matrix.h
+++ Matrix.h
@@ -22,12 +22,15 @@ typedef vector<vector<float>> Mat;
 using namespace std;
 class Matrix
 {
-private:
-    Mat mat;
-
 public:
+    Mat mat;
+    size_t row, col;
     // 构造函数
-    Matrix(size_t row, size_t col)
+    Matrix() : row(1), col(1)
+    {
+        mat[0][0] = 0;
+    }
+    Matrix(size_t row, size_t col) : row(row), col(col)
     {
         if (row <= 0 || col <= 0)
             cout << "row or col is illegal" << endl;
@@ -38,7 +41,7 @@ public:
         }
     }
     // 赋值构造函数
-    Matrix(size_t row, size_t col, double val)
+    Matrix(size_t row, size_t col, double val) : row(row), col(col)
     {
         if (row <= 0 || col <= 0)
             cout << "row or col is illegal" << endl;
@@ -52,6 +55,20 @@ public:
             }
         }
     }
+
+    Matrix(const Matrix &a)
+    {
+        mat.resize(a.row);
+        for (int i = 0; i < mat.size(); ++i)
+        {
+            mat[i].resize(a.col); // 设置列数
+            for (int j = 0; j < a.col; j++)
+            {
+                mat[i][j] = a.mat[i][j];
+            }
+        }
+    }
+
     // Initial the value through Gaussian distribution
     void Gaussian_init()
     {
@@ -63,221 +80,41 @@ public:
             }
         }
     }
-    // 矩阵二维卷积
-    static Matrix conv2d(const Mat &A, const Mat &kernel)
-    {
-        if (kernel.size() > A.size() || kernel[0].size() > A[0].size())
-            cout << "kernel size is illegal" << endl;
-        int row = A.size();
-        int col = A[0].size();
-        int kernel_row = kernel.size();
-        int kernel_col = kernel[0].size();
-        int out_row = row - kernel_row + 1; // 输出矩阵的行列数
-        int out_col = col - kernel_col + 1; // 输出矩阵的行列数
-        Matrix out(out_row, out_col, 0);
-        for (int i = 0; i < out_row; i++)
-        {
-            for (int j = 0; j < out_col; j++)
-            {
-                for (int m = 0; m < kernel_row; m++)
-                {
-                    for (int n = 0; n < kernel_col; n++)
-                    {
-                        out.mat[i][j] += A[i + m][j + n] * kernel[m][n];
-                    }
-                }
-            }
-        }
-        return out;
-    }
-    // 矩阵加法,A加上B,结果存储在add中
-    void MatrixAdd(const Matrix &A, const Matrix &B, Matrix &add)
-    {
-        if (A.mat.size() != B.mat.size() || A.mat[0].size() != B.mat[0].size())
-            cout << "MatrixAdd: Matrix size is not equal" << endl;
-        int row = A.mat.size();
-        int col = A.mat[0].size();
-        add.mat.resize(row); // 设置行数
-        for (int i = 0; i < row; ++i)
-        {
-            add.mat[i].resize(col); // 设置列数
-            for (int j = 0; j < col; ++j)
-            {
-                add.mat[i][j] = A.mat[i][j] + B.mat[i][j];
-            }
-        }
-    }
-    // 广播加法
-    void MatrixAdd(const Matrix &A, double val)
-    {
-        for (int i = 0; i < A.mat.size(); i++)
-        {
-            for (int j = 0; j < A.mat[0].size(); j++)
-            {
-                mat[i][j] = A.mat[i][j] + val;
-            }
-        }
-    }
-    // 加法重载，将B加到A中
-    void MatrixAdd(Matrix &A, const Matrix &B)
-    {
-        if (A.mat.size() != B.mat.size() || A.mat[0].size() != B.mat[0].size())
-            cout << "MatrixAdd: Matrix size is not equal" << endl;
-        int row = A.mat.size();
-        int col = A.mat[0].size();
-        for (int i = 0; i < row; ++i)
-        {
-            for (int j = 0; j < col; ++j)
-            {
-                A.mat[i][j] += B.mat[i][j];
-            }
-        }
-    }
-    // 矩阵减法,A减去B,结果存储在sub中
-    void MatrixMinus(const Matrix &A, const Matrix &B, Matrix &R)
-    {
-        if (A.mat.size() != B.mat.size() || A.mat[0].size() != B.mat[0].size())
-            cout << "MatrixMinus: Matrix size is not equal" << endl;
-        int row = A.mat.size();
-        int col = A.mat[0].size();
-        R.mat.resize(row); // 设置行数
-        for (int i = 0; i < row; ++i)
-        {
-            R.mat[i].resize(col); // 设置列数
-            for (int j = 0; j < col; ++j)
-            {
-                R.mat[i][j] = A.mat[i][j] - B.mat[i][j];
-            }
-        }
-    }
-    // 矩阵间乘法
-    void MatrixMul(const Matrix &A, const Matrix &B, Matrix &mul)
-    {
-        if (A.mat[0].size() != B.mat.size())
-            cout << "MatrixMul: Matrix size is not equal" << endl;
-        int row = A.mat.size();
-        int col = B.mat[0].size();
-        int mid = A.mat[0].size();
-        mul.mat.resize(row); // 设置行数
-        for (int i = 0; i < row; ++i)
-        {
-            mul.mat[i].resize(col); // 设置列数
-            for (int j = 0; j < col; ++j)
-            {
-                mul.mat[i][j] = 0;
-                for (int k = 0; k < mid; ++k)
-                {
-                    mul.mat[i][j] += A.mat[i][k] * B.mat[k][j];
-                }
-            }
-        }
-    }
-    // 矩阵与标量相乘
-    void MatrixMul(Matrix &A, double val)
-    {
-        for (int i = 0; i < A.mat.size(); i++)
-        {
-            for (int j = 0; j < A.mat[0].size(); j++)
-            {
-                A.mat[i][j] *= val;
-            }
-        }
-    }
-    // 矩阵与标量相乘
-    void MatrixMul(const Matrix &A, double val, Matrix &R)
-    {
-        R.mat.resize(A.mat.size()); // 设置行数
-        for (int i = 0; i < A.mat.size(); ++i)
-        {
-            R.mat[i].resize(A.mat[0].size()); // 设置列数
-            for (int j = 0; j < A.mat[0].size(); ++j)
-            {
-                R.mat[i][j] = A.mat[i][j] * val;
-            }
-        }
-    }
-    // 矩阵点乘
-    void MatrixDotMul(const Matrix &A, const Matrix &B, Matrix &R)
-    {
-        if (A.mat.size() != B.mat.size() || A.mat[0].size() != B.mat[0].size())
-            cout << "MatrixDotMul: Matrix size is not equal" << endl;
-        int row = A.mat.size();
-        int col = A.mat[0].size();
-        R.mat.resize(row); // 设置行数
-        for (int i = 0; i < row; ++i)
-        {
-            R.mat[i].resize(col); // 设置列数
-            for (int j = 0; j < col; ++j)
-            {
-                R.mat[i][j] = A.mat[i][j] * B.mat[i][j];
-            }
-        }
-    }
-    // 矩阵池化-最大池化
-    // pool_size:池化核大小
-    // stride:步长
-    void MatrixPool(const Matrix &A, Matrix &R, int pool_size, int stride)
-    {
-        int row = A.mat.size();
-        int col = A.mat[0].size();
-        int out_row = (row - pool_size) / stride + 1; // 输出矩阵的行列数
-        int out_col = (col - pool_size) / stride + 1; // 输出矩阵的行列数
-        R.mat.resize(out_row);                        // 设置行数
-        for (int i = 0; i < out_row; ++i)
-        {
-            R.mat[i].resize(out_col); // 设置列数
-            for (int j = 0; j < out_col; ++j)
-            {
-                R.mat[i][j] = 0;
-                for (int m = 0; m < pool_size; ++m)
-                {
-                    for (int n = 0; n < pool_size; ++n)
-                    {
-                        R.mat[i][j] = max(R.mat[i][j], A.mat[i * stride + m][j * stride + n]);
-                    }
-                }
-            }
-        }
-    }
     // 矩阵转置
-    void MatrixTranspose(const Matrix &A, Matrix &R)
+    Matrix MatrixTranspose()
     {
-        int row = A.mat.size();
-        int col = A.mat[0].size();
-        R.mat.resize(col); // 设置行数
+        Matrix R(row, col);
         for (int i = 0; i < col; ++i)
         {
-            R.mat[i].resize(row); // 设置列数
             for (int j = 0; j < row; ++j)
             {
-                R.mat[i][j] = A.mat[j][i];
+                R.mat[i][j] = mat[j][i];
             }
         }
+        return R;
     }
     // 矩阵旋转180度
-    void MatrixRotate180(const Matrix &A, Matrix &R)
+    Matrix MatrixRotate180()
     {
-        int row = A.mat.size();
-        int col = A.mat[0].size();
-        R.mat.resize(row); // 设置行数
+        Matrix R(row, col);
         for (int i = 0; i < row; ++i)
         {
-            R.mat[i].resize(col); // 设置列数
             for (int j = 0; j < col; ++j)
             {
-                R.mat[i][j] = A.mat[row - i - 1][col - j - 1];
+                R.mat[i][j] = mat[row - i - 1][col - j - 1];
             }
         }
+        return R;
     }
     // 求和
-    double MatrixSum(const Matrix &A)
+    double MatrixSum()
     {
         double sum = 0;
-        for (int i = 0; i < A.mat.size(); i++)
+        for (int i = 0; i < mat.size(); i++)
         {
-            for (int j = 0; j < A.mat[0].size(); j++)
+            for (int j = 0; j < mat[0].size(); j++)
             {
-                sum += A.mat[i][j];
+                sum += mat[i][j];
             }
         }
         return sum;
@@ -285,14 +122,205 @@ public:
     // 打印矩阵
     void MatrixPrint()
     {
-        for (int i = 0; i < mat.size(); i++)
+        for (int i = 0; i < row; i++)
         {
-            for (int j = 0; j < mat[0].size(); j++)
+            for (int j = 0; j < col; j++)
             {
                 cout << mat[i][j] << " ";
             }
             cout << endl;
         }
     }
+    static Matrix conv2d(const Mat &A, const Mat &kernel);
+    Matrix MatrixAdd(const Matrix &B);
+    Matrix MatrixAdd(double val);
+    Matrix MatrixMinus(const Matrix &B);
+    Matrix MatrixMinus(double val)
+    {
+        return MatrixAdd(-val);
+    }
+    Matrix MatrixMul(const Matrix &B);
+    Matrix MatrixMul(double val);
+    Matrix MatrixDotMul(const Matrix &B);
+    Matrix MatrixPool(int pool_size, int stride);
+
+    Matrix operator+(const Matrix &B)
+    {
+        return MatrixAdd(B);
+    }
+    Matrix operator+(double val)
+    {
+        return MatrixAdd(val);
+    }
+    Matrix operator-(const Matrix &B)
+    {
+        return MatrixMinus(B);
+    }
+    Matrix operator-(double val)
+    {
+        return MatrixMinus(val);
+    }
+    Matrix operator*(const Matrix &B)
+    {
+        return MatrixMul(B);
+    }
+    Matrix operator*(double val)
+    {
+        return MatrixMinus(val);
+    }
+    Matrix operator^(const Matrix &B)
+    {
+        return MatrixDotMul(B);
+    }
 };
+
+// 矩阵二维卷积
+Matrix Matrix::conv2d(const Mat &A, const Mat &kernel)
+{
+    if (kernel.size() > A.size() || kernel[0].size() > A[0].size())
+        cout << "kernel size is illegal" << endl;
+    int row = A.size();
+    int col = A[0].size();
+    int kernel_row = kernel.size();
+    int kernel_col = kernel[0].size();
+    int out_row = row - kernel_row + 1; // 输出矩阵的行列数
+    int out_col = col - kernel_col + 1; // 输出矩阵的行列数
+    Matrix out(out_row, out_col, 0);
+    for (int i = 0; i < out_row; i++)
+    {
+        for (int j = 0; j < out_col; j++)
+        {
+            for (int m = 0; m < kernel_row; m++)
+            {
+                for (int n = 0; n < kernel_col; n++)
+                {
+                    out.mat[i][j] += A[i + m][j + n] * kernel[m][n];
+                }
+            }
+        }
+    }
+    return out;
+}
+// 矩阵加法,A加上B,结果存储在add中
+Matrix Matrix::MatrixAdd(const Matrix &B)
+{
+    if (row != B.row || col != B.col)
+        cout << "MatrixAdd: Matrix size is not equal" << endl;
+    Matrix add(row, col);
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            add.mat[i][j] = mat[i][j] + B.mat[i][j];
+        }
+    }
+    return add;
+}
+// 广播加法
+Matrix Matrix::MatrixAdd(double val)
+{
+    Matrix A(row, col);
+    for (int i = 0; i < A.mat.size(); i++)
+    {
+        for (int j = 0; j < A.mat[0].size(); j++)
+        {
+            A.mat[i][j] = mat[i][j] + val;
+        }
+    }
+    return A;
+}
+
+// 矩阵减法,A减去B,结果存储在sub中
+Matrix Matrix::MatrixMinus(const Matrix &B)
+{
+    if (row != B.row || col != B.col)
+        cout << "MatrixAdd: Matrix size is not equal" << endl;
+    Matrix R(row, col);
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            R.mat[i][j] = mat[i][j] - B.mat[i][j];
+        }
+    }
+    return R;
+}
+// 矩阵间乘法
+Matrix Matrix::MatrixMul(const Matrix &B)
+{
+    if (row != B.row || col != B.col)
+        cout << "MatrixAdd: Matrix size is not equal" << endl;
+    Matrix mul(row, col);
+    int col = B.mat[0].size();
+    int mid = mat[0].size();
+    mul.mat.resize(row); // 设置行数
+    for (int i = 0; i < row; ++i)
+    {
+        mul.mat[i].resize(col); // 设置列数
+        for (int j = 0; j < col; ++j)
+        {
+            mul.mat[i][j] = 0;
+            for (int k = 0; k < mid; ++k)
+            {
+                mul.mat[i][j] += mat[i][k] * B.mat[k][j];
+            }
+        }
+    }
+    return mul;
+}
+// 矩阵与标量相乘
+Matrix Matrix::MatrixMul(double val)
+{
+    Matrix result(row, col);
+    for (int i = 0; i < mat.size(); i++)
+    {
+        for (int j = 0; j < mat[0].size(); j++)
+        {
+            result.mat[i][j] = mat[i][j] * val;
+        }
+    }
+    return result;
+}
+// 矩阵点乘
+Matrix Matrix::MatrixDotMul(const Matrix &B)
+{
+    if (row != B.row || col != B.col)
+        cout << "MatrixAdd: Matrix size is not equal" << endl;
+    Matrix R(row, col);
+    R.mat.resize(row); // 设置行数
+    for (int i = 0; i < row; ++i)
+    {
+        R.mat[i].resize(col); // 设置列数
+        for (int j = 0; j < col; ++j)
+        {
+            R.mat[i][j] = mat[i][j] * B.mat[i][j];
+        }
+    }
+    return R;
+}
+// 矩阵池化-最大池化
+// pool_size:池化核大小
+// stride:步长
+Matrix Matrix::MatrixPool(int pool_size, int stride)
+{
+    int out_row = (row - pool_size) / stride + 1; // 输出矩阵的行列数
+    int out_col = (col - pool_size) / stride + 1; // 输出矩阵的行列数
+    Matrix R(out_row, out_col);
+    for (int i = 0; i < out_row; ++i)
+    {
+        for (int j = 0; j < out_col; ++j)
+        {
+            R.mat[i][j] = 0;
+            for (int m = 0; m < pool_size; ++m)
+            {
+                for (int n = 0; n < pool_size; ++n)
+                {
+                    R.mat[i][j] = max(R.mat[i][j], mat[i * stride + m][j * stride + n]);
+                }
+            }
+        }
+    }
+    return R;
+}
+
 #endif // LENET_5_MATRIX_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "Matrix.h"
 
-int main() {
+int main()
+{
     // 创建两个矩阵 A 和 B
     Matrix A(3, 3, 1.0);
     Matrix B(3, 3, 2.0);
@@ -13,22 +14,20 @@ int main() {
     B.MatrixPrint();
 
     // 创建一个结果矩阵 add
-    Matrix add(3, 3);
 
     // 调用矩阵加法函数
-    add.MatrixAdd(A, B, add);
+    Matrix add=A.MatrixAdd(B);
 
     // 打印矩阵 add
     cout << "Matrix A + B:" << endl;
     add.MatrixPrint();
 
-    //创建一个乘法矩阵
-    Matrix C(3, 3);
+    // 创建一个乘法矩阵
 
-    //调用矩阵乘法函数
-    C.MatrixMul(A, B, C);
+    // 调用矩阵乘法函数
+    Matrix C=A.MatrixMul(B);
 
-    //打印矩阵C
+    // 打印矩阵C
     cout << "Matrix A * B:" << endl;
     C.MatrixPrint();
 
@@ -46,32 +45,31 @@ int main() {
     cout << "Convolution Result:" << endl;
     conv_result.MatrixPrint();
 
-    //池化层
-    Matrix pool_result(2, 2);
+    // 池化层
 
-    //调用矩阵池化函数
-    pool_result.Matrix::MatrixPool(A, pool_result, 2 , 1);
+    // 调用矩阵池化函数
+    Matrix pool_result=A.MatrixPool(2, 1);
 
-    //打印池化结果
+    // 打印池化结果
     cout << "Pooling Result:" << endl;
     pool_result.MatrixPrint();
 
-    //标量相乘
-    pool_result.MatrixMul(pool_result, 1.2);
+    // 标量相乘
+    pool_result=pool_result.MatrixMul(1.2);
 
-    //打印标量相乘结果
+    // 打印标量相乘结果
     cout << "Scalar Multiplication Result:" << endl;
     pool_result.MatrixPrint();
 
-    //广播操作
+    // 广播操作
     Matrix broadcast_result(3, 3);
-    broadcast_result.MatrixAdd(A,  22);
+    broadcast_result=broadcast_result.MatrixAdd(22);
 
-    //打印广播操作结果
+    // 打印广播操作结果
     cout << "Broadcast Result:" << endl;
     broadcast_result.MatrixPrint();
 
-    //Gaussian initual
+    // Gaussian initual
     A.Gaussian_init();
     A.MatrixPrint();
 
