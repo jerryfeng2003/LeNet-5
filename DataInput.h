0f92145c4f3489d6853b5d4c73d70f3b1910b8a2
jerryfeng2003 <jerryfeng2003@gmail.com>
2023-10-27 22:01:30 +0800

    finish back propagation

--- DataInput.h
+++ DataInput.h
@@ -10,29 +10,29 @@
 #include <fstream>
 #include <vector>
 
-#define INPUT_HEIGHT 32
-#define INPUT_WIDTH  32//输入图片的长宽
-#define MNISI_HEIGHT 28
-#define MNISI_WIDTH  28//MNIST图片的长宽
-#define PADDING_SIZE 2//填充大小
-#define OUTPUT_SIZE 10//输出大小
-#define TRAIN_NUM 3000//训练集大小
-#define TEST_NUM  100//测试集大小
+const int INPUT_HEIGHT = 32;
+const int INPUT_WIDTH = 32; // 输入图片的长宽
+const int MNISI_HEIGHT = 28;
+const int MNISI_WIDTH = 28; // MNIST图片的长宽
+const int PADDING_SIZE = 2; // 填充大小
+const int OUTPUT_SIZE = 10; // 输出大小
+const int TRAIN_NUM = 3000; // 训练集大小
+const int TEST_NUM = 100;   // 测试集大小
 using namespace std;
 
 struct Point
-        {
+{
     Matrix image;
     Matrix label;
     Point() { ; }
-    Point(const char * image, uint8_t num )
+    Point(const char *image, uint8_t num)
     {
         this->image = Matrix(INPUT_HEIGHT, INPUT_WIDTH);
-        for(int i = 0; i < INPUT_HEIGHT; i++)
+        for (int i = 0; i < INPUT_HEIGHT; i++)
         {
-            for(int j = 0; j < INPUT_WIDTH; j++)
+            for (int j = 0; j < INPUT_WIDTH; j++)
             {
-                this->image.mat[i+PADDING_SIZE][j+PADDING_SIZE] =(uint8_t) image[i*MNISI_HEIGHT+j];
+                this->image.mat[i + PADDING_SIZE][j + PADDING_SIZE] = (uint8_t)image[i * MNISI_HEIGHT + j];
             }
         }
         this->label = Matrix(OUTPUT_SIZE, 1);
@@ -40,38 +40,38 @@ struct Point
     }
 };
 
-inline void ReadData(vector<Point>&Train, vector<Point>&Test)
+inline void ReadData(vector<Point> &Train, vector<Point> &Test)
 {
-    char rubbish[16];//不用的数据，包括图片数量，图片长宽等
-    ifstream train_image("./Datas/train-images.idx3-ubyte", ios::binary | ios::in );
+    char rubbish[16]; // 不用的数据，包括图片数量，图片长宽等
+    ifstream train_image("./Datas/train-images.idx3-ubyte", ios::binary | ios::in);
     ifstream train_label("./Datas/train-labels.idx1-ubyte", ios::binary | ios::in);
 
     train_image.read(rubbish, 16);
     train_label.read(rubbish, 8);
 
-    for(int i = 0; i < TRAIN_NUM; i++)
+    for (int i = 0; i < TRAIN_NUM; i++)
     {
-        char image[MNISI_HEIGHT*MNISI_WIDTH];
+        char image[MNISI_HEIGHT * MNISI_WIDTH];
         uint8_t num;
-        train_image.read(image, MNISI_HEIGHT*MNISI_WIDTH);
-        train_label.read((char*)&num, 1);//标签的字节数为1
+        train_image.read(image, MNISI_HEIGHT * MNISI_WIDTH);
+        train_label.read((char *)&num, 1); // 标签的字节数为1
         Train.push_back(Point(image, num));
     }
 
-    ifstream test_image("./Datas/t10k-images.idx3-ubyte", ios::binary | ios::in );
-    ifstream test_label("./Datas/t10k-labels.idx1-ubyte", ios::binary | ios::in );
+    ifstream test_image("./Datas/t10k-images.idx3-ubyte", ios::binary | ios::in);
+    ifstream test_label("./Datas/t10k-labels.idx1-ubyte", ios::binary | ios::in);
 
     test_image.read(rubbish, 16);
     test_label.read(rubbish, 8);
 
-    for(int i = 0; i < TEST_NUM; i++)
+    for (int i = 0; i < TEST_NUM; i++)
     {
-        char image[MNISI_HEIGHT*MNISI_WIDTH];
+        char image[MNISI_HEIGHT * MNISI_WIDTH];
         uint8_t num;
-        test_image.read(image, MNISI_HEIGHT*MNISI_WIDTH);
-        test_label.read((char*)&num, 1);//标签的字节数为1
+        test_image.read(image, MNISI_HEIGHT * MNISI_WIDTH);
+        test_label.read((char *)&num, 1); // 标签的字节数为1
         Test.push_back(Point(image, num));
     }
 }
 
-#endif //LENET_5_DATAINPUT_H
+#endif // LENET_5_DATAINPUT_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "Matrix.h"
 #include "DataInput.h"
-#include <>
-//使用OpenCV库
-//#include <opencv2/opencv.hpp>
+#include "Network.h"
 
 using namespace std;
 
+int main()
+{
+    Point input;
+    input.image.resize(32,32);
+    input.label.resize(10,1);
+    LeNet_5 model;
+    // model.Network_init();
+    // model.ForwardPropagation(input);
+    return 0;
+}
