ddff93449b0ddf19111b04a71130063499fe87e1
Xianjin_Wu <1278117059@qq.com>
2023-10-28 11:14:06 +0800

    DataInput

--- DataInput.h
+++ DataInput.h
@@ -16,8 +16,9 @@
 #define MNISI_WIDTH  28//MNIST图片的长宽
 #define PADDING_SIZE 2//填充大小
 #define OUTPUT_SIZE 10//输出大小
-#define TRAIN_NUM 20//训练集大小
-#define TEST_NUM  20//测试集大小
+#define TRAIN_SIZE 60000//训练集大小
+#define TEST_SIZE  10000//测试集大小
+
 using namespace std;
 
 struct Point
@@ -25,38 +26,44 @@ struct Point
     Matrix image;
     Matrix label;
     //构造函数
-    Point(const char * Image, uint8_t num):image(INPUT_HEIGHT, INPUT_WIDTH, 0), label(OUTPUT_SIZE, 1)
-    {
-        this->image = Matrix(INPUT_HEIGHT, INPUT_WIDTH);
-        for(int i = 0; i < MNISI_HEIGHT; i++)
-        {
-            for(int j = 0; j < MNISI_HEIGHT; j++)
-            {
-                this->image.mat[i+PADDING_SIZE][j+PADDING_SIZE] =(uint8_t) Image[i*MNISI_HEIGHT+j];
-            }
-        }
-        this->label = Matrix(OUTPUT_SIZE, 1);
-        label.mat[num][0] = 1;
-    }
+    Point(int num, string URL, int flag);
 };
 
-//把大端数据转换为我们常用的小端数据
-uint32_t swap_endian(uint32_t val)
+//flag: 0表示训练集，1表示测试集
+Point:: Point(int num, const string URL, int flag):image(INPUT_HEIGHT, INPUT_WIDTH, 0), label(OUTPUT_SIZE, 1)
 {
-    val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
-    return (val << 16) | (val >> 16);
-}
+    uint32_t image_number[4];   //魔数, 图片数量, 行数, 列数
+    uint32_t label_number[2];   //魔数, 标签数量
+    string image_url;
+    string label_url;
+    if (flag == 0)
+    {
+        image_url = URL + "train-images.idx3-ubyte";
+        label_url = URL + "train-labels.idx1-ubyte";
+        if(num < 0 || num > TRAIN_SIZE)
+        {
+            cout<<"num error!"<<endl;
+            exit(0);
+        }
+    }
+    else if (flag == 1)
+    {
+        image_url = URL + "t10k-images.idx3-ubyte";
+        label_url = URL + "t10k-labels.idx1-ubyte";
+        if(num < 0 || num > TEST_SIZE)
+        {
+            cout<<"num error!"<<endl;
+            exit(0);
+        }
+    }
+    else
+    {
+        cout<<"flag error!"<<endl;
+        exit(0);
+    }
 
-inline void ReadData(vector<Point>&Train, vector<Point>&Test)
-{
-    uint32_t magic_number;      //魔数
-    uint32_t number_of_images;  //图片数量
-    uint32_t number_of_labels;  //标签数量
-    uint32_t number_of_rows;    //行数
-    uint32_t number_of_columns; //列数
-
-    ifstream train_image("C:\\Users\\Hyper\\CLionProjects\\LeNet-5\\Datas\\train-images.idx3-ubyte", ios::binary | ios::in );
-    ifstream train_label("C:\\Users\\Hyper\\CLionProjects\\LeNet-5\\Datas\\train-labels.idx1-ubyte", ios::binary | ios::in);
+    ifstream train_image(image_url, ios::binary | ios::in );
+    ifstream train_label(label_url, ios::binary | ios::in );
 
     if(!train_image.is_open() || !train_label.is_open())//打开文件失败
     {
@@ -64,109 +71,27 @@ inline void ReadData(vector<Point>&Train, vector<Point>&Test)
         exit(0);
     }
 
-    train_image.read((char*)&magic_number, 4);//魔数的字节数为4
-    magic_number = swap_endian(magic_number);//转换为小端数据
+    train_image.read((char*)&image_number, sizeof (image_number));
+    train_label.read((char*)&label_number, sizeof (label_number));
 
-    if(magic_number != 2051)//魔数不对
+    char *temp_image = new char[MNISI_HEIGHT*MNISI_WIDTH];
+    //移动文件指针，读取第n张图片
+    train_image.seekg(num*MNISI_HEIGHT*MNISI_WIDTH, ios::cur);
+    train_image.read(temp_image, MNISI_HEIGHT*MNISI_WIDTH);
+    char temp_label;
+    //移动文件指针，读取第n个标签
+    train_label.seekg(num, ios::cur);
+    train_label.read(&temp_label, 1);//标签的字节数为1
+    //存入Train
+    this->image = Matrix(INPUT_HEIGHT, INPUT_WIDTH);
+    for(int i = 0; i < MNISI_HEIGHT; i++)
     {
-        cout<<"Magic number error!"<<endl;
-        exit(0);
+        for(int j = 0; j < MNISI_HEIGHT; j++)
+        {
+            this->image.mat[i+PADDING_SIZE][j+PADDING_SIZE] =(uint8_t)temp_image[i*MNISI_HEIGHT+j];
+        }
     }
-
-    train_label.read((char*)&magic_number, 4);//魔数的字节数为4
-    magic_number = swap_endian(magic_number);//转换为小端数据
-
-    if(magic_number != 2049)//魔数不对
-    {
-        cout<<"Magic number error!"<<endl;
-        exit(0);
-    }
-
-    train_image.read(reinterpret_cast<char*>(&number_of_images), 4);//图片数量的字节数为4
-    number_of_images = swap_endian(number_of_images);//转换为小端数据
-    train_label.read(reinterpret_cast<char*>(&number_of_labels), 4);//标签数量的字节数为4
-    number_of_labels = swap_endian(number_of_labels);//转换为小端数据
-
-    //判断图片数量和标签数量是否相等
-    if(number_of_images != number_of_labels)
-    {
-        cout<<"Number of images and labels are not equal!"<<endl;
-        exit(0);
-    }
-
-    //读行数和列数
-    train_image.read(reinterpret_cast<char*>(&number_of_rows), 4);
-    number_of_rows = swap_endian(number_of_rows);
-    train_image.read(reinterpret_cast<char*>(&number_of_columns), 4);
-    number_of_columns = swap_endian(number_of_columns);
-
-    //读取图像
-    for(int i = 0; i < number_of_images; i++)
-    {
-        char *image = new char[number_of_rows*number_of_columns];
-        train_image.read(image, number_of_rows*number_of_columns);
-        char label;
-        train_label.read(&label, 1);//标签的字节数为1
-        Train.push_back(Point(image, label));
-        delete [] image;
-    }
-
-    ifstream test_image("C:\\Users\\Hyper\\CLionProjects\\LeNet-5\\Datas\\t10k-images.idx3-ubyte", ios::binary | ios::in );
-    ifstream test_label("C:\\Users\\Hyper\\CLionProjects\\LeNet-5\\Datas\\t10k-labels.idx1-ubyte", ios::binary | ios::in );
-
-    if(!test_image.is_open() || !test_label.is_open())//打开文件失败
-    {
-        cout<<"Open file failed!"<<endl;
-        exit(0);
-    }
-
-    test_image.read((char*)&magic_number, 4);//魔数的字节数为4
-    magic_number = swap_endian(magic_number);//转换为小端数据
-
-    if(magic_number != 2051)//魔数不对
-    {
-        cout<<"Magic number error!"<<endl;
-        exit(0);
-    }
-
-    test_label.read((char*)&magic_number, 4);//魔数的字节数为4
-    magic_number = swap_endian(magic_number);//转换为小端数据
-
-    if(magic_number != 2049)//魔数不对
-    {
-        cout<<"Magic number error!"<<endl;
-        exit(0);
-    }
-
-    test_image.read(reinterpret_cast<char*>(&number_of_images), 4);//图片数量的字节数为4
-    number_of_images = swap_endian(number_of_images);//转换为小端数据
-    test_label.read(reinterpret_cast<char*>(&number_of_labels), 4);//标签数量的字节数为4
-    number_of_labels = swap_endian(number_of_labels);//转换为小端数据
-
-    //判断图片数量和标签数量是否相等
-    if(number_of_images != number_of_labels)
-    {
-        cout<<"Number of images and labels are not equal!"<<endl;
-        exit(0);
-    }
-
-    //读行数和列数
-    test_image.read(reinterpret_cast<char*>(&number_of_rows), 4);
-    number_of_rows = swap_endian(number_of_rows);
-    test_image.read(reinterpret_cast<char*>(&number_of_columns), 4);
-    number_of_columns = swap_endian(number_of_columns);
-
-    //读取图像
-    for(int i = 0; i < number_of_images; i++)
-    {
-        char *image = new char[number_of_rows*number_of_columns];
-        test_image.read(image, number_of_rows*number_of_columns);
-        char label;
-        test_label.read(&label, 1);//标签的字节数为1
-        Test.push_back(Point(image, label));
-        delete [] image;
-    }
-
+    this->label = Matrix(OUTPUT_SIZE, 1);
+    this->label.mat[(uint8_t)temp_label][0] = 1;
 }
-
 #endif //LENET_5_DATAINPUT_H
--- main.cpp
+++ main.cpp
@@ -10,19 +10,21 @@ int main()
     cout<<"Hello World!"<<endl;
     cout<<"This is a test for the Network!"<<endl;
 
-    vector<Point> Train;
-    vector<Point> Test;
+    //绝对路径C:\Users\Hyper\CLionProjects\LeNet-5\Datas\t10k-images.idx3-ubyte改为相对路径
+    struct Point Train(59999, "../Datas/", 0);
 
-    ReadData(Train, Test);
     //显示图片
     cv::Mat image(32, 32, CV_8UC1);
 
     //先转换train[i].image.mat的数据类型,从float转换为uint8_t
     for (int j = 0; j < 32; ++j) {
         for (int k = 0; k < 32; ++k) {
-            image.at<uchar>(j, k) = (uint8_t)Train[0].image.mat[j][k];
+            image.at<uchar>(j, k) = (uint8_t)Train.image.mat[j][k];
         }
     }
+
+    //放大图片
+    cv::resize(image, image, cv::Size(256, 256), 0, 0, cv::INTER_NEAREST);
 
     cv:: imshow("image", image);
     cv:: waitKey(0);
